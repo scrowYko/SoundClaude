9ec469cba8541aa5f567f517b088444ed0631f46
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-10-23 09:53:07 -0300

    finalizando rota de registro e criação do bd

---
+++ server/db.js
@@ -0,0 +1,54 @@
+import Sequelize from 'sequelize'
+
+const sequelize = new Sequelize(
+    'spotfake', //nome do db
+    'postgres', //usuario
+    'postgres', //senha
+    {
+        host: 'localhost', //endereço
+        port: 5432, //porta
+        dialect: 'postgres' //banco usado
+    }
+)
+const User = sequelize.define('user', {
+    nome: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    sobrenome: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    dataNascimento: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    senha: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false,
+    },
+    status: {
+        type: Sequelize.DataTypes.ENUM('ativo', 'inativo'),
+        allowNull: false,
+        defaultValue: 'inativo'
+    }
+})
+
+const criarTabelas = () => {
+    sequelize.authenticate().then(() => {
+        console.log('conectou')
+    })
+        .catch((err) => {
+            console.log(err)
+        })
+    sequelize.sync({ force: true }).then(() => {
+        console.log('tabela criada')
+    })
+}
+
+export { User, sequelize, criarTabelas };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,32 @@
 import Express from "express";
+import { User, criarTabelas } from "./db.js";
+import bcryptjs from 'bcryptjs'
+ 
 
 const app = Express()
 app.use(Express.json())
 
-app.get('/pegar', function (req, res) {
-    res.send('Enviar mensagem')
-})
 
-app.get('/pegar2', function (req, res) {
-    res.send('Enviar outra mensagem')
-})
+const verificarTabelaUser = async () => {
+    try {
+        const result = await User.findOne();
+        if (result) {
+            console.log('Tabela "user" existe');
+            return true;
+        } else {
+            console.log('Tabela "user" não existe');
+            criarTabelas() 
+            return false;
+        }
+    } catch (error) {
+        console.log('Erro ao verificar tabela "user"');
+        return false;
+    }
+};
 
-app.post('/registro', function(req, res) {
+//verificarTabelaUser()
+
+app.post('/registro', async function(req, res) {
     console.log(req.body)
     //get infos from body
     try {
@@ -20,20 +35,32 @@ app.post('/registro', function(req, res) {
         if(!nome || !sobrenome || !dataNascimento || !email || !senha){
             return res.status(406).send({ message: 'Preencha todos os campos' })
         }
-        console.log('criar user');
+        //veridy if the user already exists
+        if( await User.findOne({where:{email: email}})){
+            return res.status(418).send({ message: 'Email já cadastrado' })
+        }
+        //encrypt password
+        const  senhaCriptografada = bcryptjs.hashSync(senha, 10)
+
+    //create user in database  
+        let novoUsuario = User.create({
+            nome: nome,
+            sobrenome: sobrenome,
+            dataNascimento: dataNascimento,
+            email: email,
+            senha:  senhaCriptografada
+        })
+        novoUsuario.save()
         return res.status(201).send({message: 'Usuário criado'})
     } catch (error) {
         return res.status(error).send({message: 'Erro ao conectar com banco de dados'})
     }
+    
     //encrypt  the password
-    //create user in database   
+     
     //response to client
 })
 
-// const server = createServer((req, res) => {
-//   res.write("Hello world");
 
-//   return res.end()
-// });
 
 app.listen(8000);
