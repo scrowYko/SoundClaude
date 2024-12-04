1a55f287c4154151bf12ec715cf50ef8e56964d4
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-12-04 09:08:36 -0300

    first

--- server/db.js
+++ server/db.js
@@ -1,58 +1,63 @@
-import Sequelize from 'sequelize'
+import Sequelize from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const sequelize = new Sequelize(
-    'spotfake', //nome do db
-    'postgres', //usuario
-    'postgres', //senha
-    {
-        host: 'localhost', //endereço
-        port: 5432, //porta
-        dialect: 'postgres' //banco usado
-    }
-)
-const User = sequelize.define('user', {
-    nome: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
-    },
-    sobrenome: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    dataNascimento: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false
-    },
-    senha: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: false,
-    },
-    status: {
-        type: Sequelize.DataTypes.ENUM('ativo', 'inativo'),
-        allowNull: false,
-        defaultValue: 'inativo'
-    },
-    imagem_perfil: {
-        type: Sequelize.DataTypes.STRING,
-        allowNull: true
-    }
-})
+  process.env.dbname, //nome do db
+  process.env.dbusername, //usuario
+  process.env.dbpassword, //senha
+  {
+    host: process.env.dbhost, //endereço
+    port: process.env.dbport, //porta
+    dialect: "postgres", //banco usado
+  }
+);
+
+const User = sequelize.define("user", {
+  nome: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  sobrenome: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  dataNascimento: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  senha: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.DataTypes.ENUM("ativo", "inativo"),
+    allowNull: false,
+    defaultValue: "inativo",
+  },
+  imagem_perfil: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: true,
+  },
+});
 
 const criarTabelas = () => {
-    sequelize.authenticate().then(() => {
-        console.log('conectou')
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("conectou");
     })
-        .catch((err) => {
-            console.log(err)
-        })
-    sequelize.sync({ force: true }).then(() => {
-        console.log('tabela criada')
-    })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+  sequelize.sync({ force: true }).then(() => {
+    console.log("tabela criada");
+  });
+};
 
 export { User, sequelize, criarTabelas };
--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import Express from "express";
 import cors from 'cors'
 import { rotas_autenticacao } from "./routes/rotas_autenticacao.js";
 import { rotas_usuario } from "./routes/rotas_usuario.js";
+import { criarTabelas } from "./db.js";
+
+
 
 const app = Express();
 app.use(Express.json());
 app.use(cors())
 app.use('/autenticacao', rotas_autenticacao)
 app.use('/usuario', rotas_usuario)
+
+criarTabelas();
 // const verificarTabelaUser = async () => {
 //   try {
 //     const result = await User.findOne();
